b9a2495bba2d8b0b92731fca77b8a87135547a25
Toby Inkster <mail@tobyinkster.co.uk>
2014-09-16 09:04:22 +0100

    more metadata improvements

--- devel.scripts/mk_changes.pl
+++ devel.scripts/mk_changes.pl
@@ -1,38 +1,44 @@
 #!/usr/bin/env perl
 
 package MakeChanges {
-
+	
 	use Moose;
-	use RDF::DOAP::ChangeSets;
+	use RDF::DOAP;
 	use RDF::TrineX::Serializer::MockTurtleSoup;
 	use RDF::TrineX::Parser::Pretdsl;
 	use Path::Class qw( file dir );
 	use URI::file;
-
+	
 	has changes_destination => (
 		is         => 'ro',
 		isa        => 'Path::Class::File',
 		default    => sub { file('Changes') },
 	);
-
+	
 	has doap_destination => (
 		is         => 'ro',
 		isa        => 'Path::Class::File',
 		default    => sub { file('doap.ttl') },
 	);
-
+	
 	has input_dir => (
 		is         => 'ro',
 		isa        => 'Path::Class::Dir',
 		default    => sub { dir('meta') },
 	);
-
+	
 	has input_model => (
 		is         => 'ro',
 		isa        => 'RDF::Trine::Model',
 		lazy_build => 1,
 	);
-
+	
+	has project => (
+		is         => 'ro',
+		isa        => 'RDF::DOAP::Project',
+		lazy_build => 1,
+	);
+	
 	sub _build_input_model {
 		my $self  = shift;
 		my $model = RDF::Trine::Model->new;
@@ -52,20 +58,19 @@ package MakeChanges {
 		return $model;
 	}
 	
+	sub _build_project {
+		my $self = shift;
+		RDF::DOAP->new->from_model($self->input_model)->project;
+	}
+	
 	sub run {
 		my $self = shift;
 		
-		my $dcs = RDF::DOAP::ChangeSets->new(
-			undef,
-			$self->input_model,
-			'current',
-		);
 		warn "Writing @{[ $self->changes_destination ]}.\n";
-		$dcs->to_file($self->changes_destination);
+		$self->changes_destination->spew($self->project->changelog);
 		
-		my $ser = RDF::TrineX::Serializer::MockTurtleSoup->new;
 		warn "Writing @{[ $self->doap_destination ]}.\n";
-		$ser->serialize_model_to_file(
+		RDF::TrineX::Serializer::MockTurtleSoup->new->serialize_model_to_file(
 			$self->doap_destination->openw,
 			$self->input_model,
 		);
